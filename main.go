ac13957890ccee7d7c7e701f8a932f7fcc62c8ad
Frankie Murillo <frankidatank@gmail.com>
2024-07-27 23:06:27 -0700

    feat(WIP): add inital cobra support and setup

--- main.go
+++ main.go
@@ -1,60 +1,39 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	_ "embed"
-	"fmt"
-
-	"github.com/frankie-mur/cpkat/links"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/frankie-mur/cpkat/cmd"
 )
 
-//go:embed schema.sql
-var ddl string
+// func run() error {
 
-func run() error {
-	ctx := context.Background()
+// 	// queries := links.New(db)
 
-	db, err := sql.Open("sqlite3", "db.sqlite")
-	if err != nil {
-		return err
-	}
+// 	// newLink, err := queries.CreateLink(ctx, links.CreateLinkParams{
+// 	// 	Name: "test",
+// 	// 	Link: "https://google.com",
+// 	// })
+// 	// if err != nil {
+// 	// 	return err
+// 	// }
+// 	// fmt.Printf("New link: %+v\n", newLink)
 
-	if _, err := db.ExecContext(ctx, ddl); err != nil {
-		return err
-	}
+// 	// link, err := queries.GetLink(ctx, newLink.ID)
+// 	// if err != nil {
+// 	// 	return err
+// 	// }
 
-	queries := links.New(db)
+// 	// fmt.Printf("Fetched link: %+v\n", link)
 
-	newLink, err := queries.CreateLink(ctx, links.CreateLinkParams{
-		Name: "test",
-		Link: "https://google.com",
-	})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("New link: %+v\n", newLink)
+// 	// allLinks, err := queries.ListLinks(ctx)
+// 	// if err != nil {
+// 	// 	return err
+// 	// }
 
-	link, err := queries.GetLink(ctx, newLink.ID)
-	if err != nil {
-		return err
-	}
+// 	// fmt.Printf("List of links: %v\n", allLinks)
 
-	fmt.Printf("Fetched link: %+v\n", link)
-
-	allLinks, err := queries.ListLinks(ctx)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("List of links: %v\n", allLinks)
-
-	return nil
-}
+// 	// return nil
+// }
 
 func main() {
-	if err := run(); err != nil {
-		panic(err)
-	}
+	cmd.Execute()
 }
